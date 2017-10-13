e0edebb0172844a52dca8f1aa9677a0981d1db40
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-10-13 23:59:39 +0200

    feat: Add Texturebuilder

---
+++ TextureBuilder.cpp
@@ -0,0 +1,36 @@
+#include "TextureBuilder.h"
+
+void loadBMP(GLuint *texture, const char *filename, int width = 1024, int height = 512, bool wrap = true) {
+  unsigned char *data;
+  FILE *file;
+
+  file = fopen(filename, "rb");
+  if (file == NULL) return;
+  data = (unsigned char *) malloc(width*height*3);
+  fread(data, width*height*3, 1, file);
+  fclose(file);
+
+  for (int i = 0; i < width*height; ++i) {
+    int index = i*3;
+    unsigned char B, R;
+    B = data[index];
+    R = data[index + 2];
+
+    data[index] = R;
+    data[index + 2] = B;
+  }
+  glGenTextures(1, texture);
+  glBindTexture(GL_TEXTURE_2D, *texture);
+  glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  gluBuild2DMipmaps(GL_TEXTURE_2D, 3, width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
+  free(data);
+}
+
+void loadBMP(GLuint *texID, const char *filename, bool wrap) {
+  loadBMP(texID, filename, 1024, 512, wrap);
+}
---
+++ TextureBuilder.h
@@ -0,0 +1,14 @@
+//
+// Created by Mohamed El Zarei on 10/13/17.
+//
+
+#include <cstdio>
+#include <cstdlib>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include "glut.h"
+#endif
+
+void loadBMP(GLuint*, const char *, bool);
+void loadBMP(GLuint*, const char *,int, int, bool);
